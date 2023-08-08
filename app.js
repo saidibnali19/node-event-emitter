789b22f01e15a38e2e838521788db599ee76a04b
Said Ali <saidibnali19@gmail.com>
2023-08-08 12:01:37 +0300

    feat: created and triggered events with arguments given to the callbacks of the event handlers

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const eventEmitter = new EventEmitter()
 eventEmitter.on("start", () => console.log("Started"))
 
 eventEmitter.emit("start")
+
+eventEmitter.on("continue", number => console.log(`Continuing with ${number}`))
+
+eventEmitter.emit("continue", 20)
+
+eventEmitter.on("end", (number1, number2) => console.log(`Ending with ${number1} and ${number2}`))
+
+eventEmitter.emit("end", 99, 100)
